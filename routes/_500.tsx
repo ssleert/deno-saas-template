1abca8130618ea3a25fe5ccbd40280c482970ee8
User Ssleert <ryasempav@technologysolutionsgeorgia.ge>
2025-04-06 07:16:55 +0300

    Fix: 500 page

---
+++ routes/_500.tsx
@@ -0,0 +1,28 @@
+import { Head } from "$fresh/runtime.ts";
+
+export default function Error404() {
+  return (
+    <>
+      <Head>
+        <title>404 - Page not found</title>
+      </Head>
+      <div class="px-4 py-8 mx-auto bg-[#86efac]">
+        <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
+          <img
+            class="my-6"
+            src="/logo.svg"
+            width="128"
+            height="128"
+            alt="the Fresh logo: a sliced lemon dripping with juice"
+          />
+          <h1 class="text-4xl font-bold">500 - Internal Server Error</h1>
+          <p class="my-4">
+            There has been an error on the server and we are unable to process
+            your request.
+          </p>
+          <a href="/" class="underline">Go back home</a>
+        </div>
+      </div>
+    </>
+  );
+}
