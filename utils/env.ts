a2ebce2c7faaa61cb57ca5a7584cce533df43c5b
User Ssleert <ryasempav@technologysolutionsgeorgia.ge>
2025-04-07 06:15:20 +0300

    Fix: pup fixes

--- utils/env.ts
+++ utils/env.ts
@@ -5,7 +5,7 @@ export default {
   PROJECT_NAME_UI: Deno.env.get("PROJECT_NAME_UI") ?? "Template",
   API_DESCRIPTION: Deno.env.get("API_DESCRIPTION") ?? "Template API",
   SERVER_URL: Deno.env.get("SERVER_URL") ?? "https://localhost:8000",
-  PORT: Deno.env.get("PUP_CLUSTER_PORT") ?? (Deno.env.get("PORT") ?? "8000"),
+  PORT: Deno.env.get("PORT") ?? "8000",
   DENO_KV_URL: Deno.env.get("DENO_KV_URL"),
   ADMIN_SECURE_KEY: Deno.env.get("ADMIN_SECURE_KEY") ?? "replaceme",
 };
