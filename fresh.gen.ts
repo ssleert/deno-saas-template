d4ada1faa3755d09d3db835ccfc8bdb7b5ffb65c
User Ssleert <ryasempav@technologysolutionsgeorgia.ge>
2025-04-07 05:11:02 +0300

    Fix: added pup

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -3,6 +3,7 @@
 // This file is automatically updated during development when running `dev.ts`.
 
 import * as $_404 from "./routes/_404.tsx";
+import * as $_500 from "./routes/_500.tsx";
 import * as $_app from "./routes/_app.tsx";
 import * as $_middleware from "./routes/_middleware.ts";
 import * as $admin_middleware from "./routes/admin__/_middleware.ts";
@@ -20,6 +21,7 @@ import type { Manifest } from "$fresh/server.ts";
 const manifest = {
   routes: {
     "./routes/_404.tsx": $_404,
+    "./routes/_500.tsx": $_500,
     "./routes/_app.tsx": $_app,
     "./routes/_middleware.ts": $_middleware,
     "./routes/admin__/_middleware.ts": $admin_middleware,
